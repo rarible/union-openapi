da7d3df7611c2d1dac49b9394d330026436a45a8
Artem Yakischik <a.yakischik@rarible.com>
2021-11-05 14:22:30 +0400

    Version 1.16-SNAPSHOT

---
+++ protocol-union-model/src/main/kotlin/com/rarible/protocol/union/dto/UnionEventTopicProvider.kt
@@ -0,0 +1,29 @@
+package com.rarible.protocol.union.dto
+
+class UnionEventTopicProvider {
+
+    companion object {
+
+        const val VERSION = "v1"
+
+        fun getCollectionTopic(environment: String): String {
+            return "protocol.$environment.union.collection"
+        }
+
+        fun getItemTopic(environment: String): String {
+            return "protocol.$environment.union.item"
+        }
+
+        fun getOwnershipTopic(environment: String): String {
+            return "protocol.$environment.union.ownership"
+        }
+
+        fun getOrderTopic(environment: String): String {
+            return "protocol.$environment.union.order"
+        }
+
+        fun getActivityTopic(environment: String): String {
+            return "protocol.$environment.union.activity"
+        }
+    }
+}
---
+++ protocol-union-model/src/main/kotlin/com/rarible/protocol/union/dto/serializer/OwnershipIdSerializer.kt
@@ -0,0 +1,17 @@
+package com.rarible.protocol.union.dto.serializer
+
+import com.fasterxml.jackson.core.JsonGenerator
+import com.fasterxml.jackson.databind.SerializerProvider
+import com.fasterxml.jackson.databind.ser.std.StdSerializer
+import com.rarible.protocol.union.dto.OwnershipIdDto
+
+object OwnershipIdSerializer : StdSerializer<OwnershipIdDto>(OwnershipIdDto::class.java) {
+
+    override fun serialize(id: OwnershipIdDto?, gen: JsonGenerator, provider: SerializerProvider) {
+        if (id == null) {
+            gen.writeNull()
+            return
+        }
+        gen.writeString(id.fullId())
+    }
+}
