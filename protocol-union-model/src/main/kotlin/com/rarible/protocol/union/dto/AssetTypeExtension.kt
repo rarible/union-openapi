cb7c0382777c5da6f3856b1eb4aaae5e24e5ea66
Roman Novikov <mystdeim@gmail.com>
2021-11-08 18:30:19 +0300

    RPN-861: add order for collection

--- protocol-union-model/src/main/kotlin/com/rarible/protocol/union/dto/AssetTypeExtension.kt
+++ protocol-union-model/src/main/kotlin/com/rarible/protocol/union/dto/AssetTypeExtension.kt
@@ -3,6 +3,7 @@ package com.rarible.protocol.union.dto
 class AssetTypeExtension(
     val isCurrency: Boolean,
     val isNft: Boolean,
+    val isCollection: Boolean,
     val contract: String,
     val itemId: ItemIdDto?
 )
