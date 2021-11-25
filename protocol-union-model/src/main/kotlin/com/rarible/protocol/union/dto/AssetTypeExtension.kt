f09cd6406235d32edbf922cdf36e4b44f2bb78f1
Artem Yakischik <83639804+yakischik@users.noreply.github.com>
2021-11-25 14:15:05 +0400

    Tezos API update (#16)
    * Tezos API update
    
    * Tezos API update

--- protocol-union-model/src/main/kotlin/com/rarible/protocol/union/dto/AssetTypeExtension.kt
+++ protocol-union-model/src/main/kotlin/com/rarible/protocol/union/dto/AssetTypeExtension.kt
@@ -1,9 +1,19 @@
 package com.rarible.protocol.union.dto
 
-class AssetTypeExtension(
+import java.math.BigInteger
+
+data class AssetTypeExtension(
     val isCurrency: Boolean,
     val isNft: Boolean,
     val isCollection: Boolean,
     val contract: String,
+    val tokenId: BigInteger?,
     val itemId: ItemIdDto?
-)
+) {
+    fun currencyAddress(): String {
+        if (!isCurrency) {
+            throw IllegalArgumentException("Not a currency AssetType: $this")
+        }
+        return tokenId?.let { "$contract:$it" } ?: contract
+    }
+}
