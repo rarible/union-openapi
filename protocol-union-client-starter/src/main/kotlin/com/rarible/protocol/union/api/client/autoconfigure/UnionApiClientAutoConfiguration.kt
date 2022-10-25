68a3a0ad111d2c16abdaf4024ff78fdc2ab4d80e
Artem Yakischik <a.yakischik@rarible.com>
2022-10-25 11:50:52 +0400

    feat: Swarm url provider removed

--- protocol-union-client-starter/src/main/kotlin/com/rarible/protocol/union/api/client/autoconfigure/UnionApiClientAutoConfiguration.kt
+++ protocol-union-client-starter/src/main/kotlin/com/rarible/protocol/union/api/client/autoconfigure/UnionApiClientAutoConfiguration.kt
@@ -1,11 +1,9 @@
 package com.rarible.protocol.union.api.client.autoconfigure
 
-import com.rarible.core.application.ApplicationEnvironmentInfo
 import com.rarible.protocol.union.api.client.CompositeWebClientCustomizer
 import com.rarible.protocol.union.api.client.DefaultUnionWebClientCustomizer
 import com.rarible.protocol.union.api.client.K8sUnionApiServiceUriProvider
 import com.rarible.protocol.union.api.client.NoopWebClientCustomizer
-import com.rarible.protocol.union.api.client.SwarmUnionApiServiceUriProvider
 import com.rarible.protocol.union.api.client.UnionApiClientFactory
 import com.rarible.protocol.union.api.client.UnionApiServiceUriProvider
 import org.springframework.beans.factory.annotation.Autowired
@@ -17,9 +15,7 @@ import org.springframework.context.annotation.Bean
 
 const val UNION_WEB_CLIENT_CUSTOMIZER = "UNION_WEB_CLIENT_CUSTOMIZER"
 
-class UnionApiClientAutoConfiguration(
-    private val applicationEnvironmentInfo: ApplicationEnvironmentInfo
-) {
+class UnionApiClientAutoConfiguration() {
 
     @Autowired(required = false)
     @Qualifier(UNION_WEB_CLIENT_CUSTOMIZER)
@@ -27,13 +23,8 @@ class UnionApiClientAutoConfiguration(
 
     @Bean
     @ConditionalOnMissingBean(UnionApiServiceUriProvider::class)
-    fun unionApiServiceUriProvider(
-        @Value("\${rarible.core.client.k8s:false}") k8s: Boolean
-    ): UnionApiServiceUriProvider {
-        return if (k8s)
-            K8sUnionApiServiceUriProvider(applicationEnvironmentInfo.name)
-        else
-            SwarmUnionApiServiceUriProvider(applicationEnvironmentInfo.name)
+    fun unionApiServiceUriProvider(): UnionApiServiceUriProvider {
+        return K8sUnionApiServiceUriProvider()
     }
 
     @Bean
