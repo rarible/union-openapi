a4d5a84f52b85e76ba4d334a0a732a2a21bc2bcd
Artem Yakischik <a.yakischik@rarible.com>
2022-10-25 12:09:34 +0400

    Revert "feat: Swarm url provider removed"
    This reverts commit 68a3a0ad111d2c16abdaf4024ff78fdc2ab4d80e.

--- protocol-union-client-starter/src/main/kotlin/com/rarible/protocol/union/api/client/autoconfigure/UnionApiClientAutoConfiguration.kt
+++ protocol-union-client-starter/src/main/kotlin/com/rarible/protocol/union/api/client/autoconfigure/UnionApiClientAutoConfiguration.kt
@@ -1,9 +1,11 @@
 package com.rarible.protocol.union.api.client.autoconfigure
 
+import com.rarible.core.application.ApplicationEnvironmentInfo
 import com.rarible.protocol.union.api.client.CompositeWebClientCustomizer
 import com.rarible.protocol.union.api.client.DefaultUnionWebClientCustomizer
 import com.rarible.protocol.union.api.client.K8sUnionApiServiceUriProvider
 import com.rarible.protocol.union.api.client.NoopWebClientCustomizer
+import com.rarible.protocol.union.api.client.SwarmUnionApiServiceUriProvider
 import com.rarible.protocol.union.api.client.UnionApiClientFactory
 import com.rarible.protocol.union.api.client.UnionApiServiceUriProvider
 import org.springframework.beans.factory.annotation.Autowired
@@ -15,7 +17,9 @@ import org.springframework.context.annotation.Bean
 
 const val UNION_WEB_CLIENT_CUSTOMIZER = "UNION_WEB_CLIENT_CUSTOMIZER"
 
-class UnionApiClientAutoConfiguration() {
+class UnionApiClientAutoConfiguration(
+    private val applicationEnvironmentInfo: ApplicationEnvironmentInfo
+) {
 
     @Autowired(required = false)
     @Qualifier(UNION_WEB_CLIENT_CUSTOMIZER)
@@ -23,8 +27,13 @@ class UnionApiClientAutoConfiguration() {
 
     @Bean
     @ConditionalOnMissingBean(UnionApiServiceUriProvider::class)
-    fun unionApiServiceUriProvider(): UnionApiServiceUriProvider {
-        return K8sUnionApiServiceUriProvider()
+    fun unionApiServiceUriProvider(
+        @Value("\${rarible.core.client.k8s:false}") k8s: Boolean
+    ): UnionApiServiceUriProvider {
+        return if (k8s)
+            K8sUnionApiServiceUriProvider(applicationEnvironmentInfo.name)
+        else
+            SwarmUnionApiServiceUriProvider(applicationEnvironmentInfo.name)
     }
 
     @Bean
