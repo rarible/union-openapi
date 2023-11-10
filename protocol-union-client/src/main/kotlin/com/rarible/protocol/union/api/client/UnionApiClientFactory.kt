95b29ab297a9865770461d61423ec3d3f39b91c4
Dmitry Saraykin <31763675+dmitry-saraykin@users.noreply.github.com>
2023-11-10 12:29:52 +0300

    feat(PT-4064): explore api (#208)
    * feat(PT-4064): explore api
    
    * feat(PT-4064): explore api
    
    * feat(PT-4064): explore api

--- protocol-union-client/src/main/kotlin/com/rarible/protocol/union/api/client/UnionApiClientFactory.kt
+++ protocol-union-client/src/main/kotlin/com/rarible/protocol/union/api/client/UnionApiClientFactory.kt
@@ -50,6 +50,10 @@ open class UnionApiClientFactory(
         return BalanceControllerApi(createApiClient())
     }
 
+    fun createSearchApiClient(): SearchControllerApi {
+        return SearchControllerApi(createApiClient())
+    }
+
     private fun createApiClient(): ApiClient {
         val jacksonMapper = ApiClient.createDefaultObjectMapper()
             .registerModule(UnionPrimitivesJacksonModule)
